66c20fb3316f8173487e56609f692361788e7175
Joules_Patman <z5312813@ad.unsw.edu.au>
2023-02-22 13:56:49 +1100

    Created scaffold for functions but need to complete stub

---
+++ src/channel.js
@@ -0,0 +1,19 @@
+// When the userId and channel Id is provided
+// Allow the user to join that specific channel
+/**
+ * @param {number} authUserId
+ * @param {number} channelId
+ */ 
+
+function channelJoinV1 (authUserId, channelId) {
+
+}
+
+//Todo
+function channelInviteV1 () {
+
+}
+
+function channelMessagesV1 () {
+
+}
---
+++ src/channels.js
@@ -0,0 +1,27 @@
+
+//Todo
+function channelsCreateV1 () {
+
+}
+
+// When the userId and channel Id is provided
+// Allow the user to join that specific channel
+/**
+ * @param {string} email
+ * @param {string} password
+ * @param {string} nameFirst
+ * @param {string} nameLast
+ * @returns {{authUserId: number}}
+ */ 
+
+function channelJoinV1 (authUserId, channelId) {
+    
+}
+//Todo
+function channelsListAllV1 () {
+
+}
+
+function channelDetailsV1 () {
+
+}
