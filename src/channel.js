c827cfcd13b94443522abea6f865781c4e26592d
Joules_Patman <z5312813@ad.unsw.edu.au>
2023-02-22 16:31:20 +1100

    Completed channelMessagesV1 and will need to review with other members

--- src/channel.js
+++ src/channel.js
@@ -18,8 +18,24 @@ function channelInviteV1 () {
  * @param {number} start
  */ 
 //Joules
-function channelMessagesV1 () {
-
+function channelMessagesV1 (authUserId, channelId, start) {
+    //Logic:
+    //Search through the dataBase for the channelId
+    //And fetch the messages for the channel
+    //let messages = dataBase.channel.find(e => e.channelId == channelId).message
+    
+    return {
+        messages: [
+            {
+                messageId: 1,
+                uId: authUserId,
+                text: "Hey everyone!",
+                timeStamp: 1000000
+            }
+        ],
+        start : start,
+        end: 1000
+    };
 }
 
 //Joules + Rachel
