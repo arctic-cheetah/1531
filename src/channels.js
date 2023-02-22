9419140ac5095c87cd185820bbce9bd81860aac3
Joules_Patman <z5312813@ad.unsw.edu.au>
2023-02-22 17:50:41 +1100

    Completed channelsListV1, need to review with other people

--- src/channels.js
+++ src/channels.js
@@ -4,11 +4,31 @@ function channelsCreateV1 () {
 
 }
 
+// Given the authUserId, 
+// return all the channels the user has enrolled in
+/**
+ * @param {number} authUserId
+ * @returns {channels: [{}]}
+ */ 
 //Joules
-function channelsListV1 () {
-
+function channelsListV1 (authUserId) {
+    //Go through the data base and fetch the user's channels
+    //let channels = dataBase.channel.find(e => e.users.find(authUserId) == authUserId)
+    return {
+        channels: [
+            {
+                channelName: "boost",
+                channelId: 1
+            }
+        ]
+    };
 }
 
 //Rachel
 function channelsListAllV1 () {
+}
+
+//Joules + Rachel
+function channelDetailsV1 () {
+
 }
