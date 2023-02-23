154f8bb8e38fc77ae12788e94beb6e954c0235d7
Rachel Doan <z5366721@ad.unsw.edu.au>
2023-02-24 01:42:07 +1100

    Add return value and parameters to stub function ChannelsCreateV1

--- src/channels.js
+++ src/channels.js
@@ -1,7 +1,19 @@
 
+// Given the authUserId, name and isPublic
+// return the channelId
+/**
+ * @param {number} authUserId
+ * @param {string} name
+ * @param {boolean} isPublic
+ * @returns {channelId: } 
+ */ 
 //Rachel
 function channelsCreateV1 () {
-
+    //Logic:
+    //Creates a channel that contains information such as
+    //authUserId, name of channel and whether it's public or not
+    
+    return {channelId: 1};
 }
 
 //Joules
