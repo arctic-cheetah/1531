b65c68cfecd85bc86d3498b68f83bb690d04a50a
Rachel Doan <z5366721@ad.unsw.edu.au>
2023-02-24 01:59:23 +1100

    Added return value and parameters to stub function channelsListAllV1

--- src/channels.js
+++ src/channels.js
@@ -9,6 +9,24 @@ function channelsListV1 () {
 
 }
 
+// Given the authUserId
+// return an array of channels for that user
+/**
+ * @param {number} authUserId
+ * @returns {channels: []} 
+ */ 
 //Rachel
 function channelsListAllV1 () {
+    //Logic:
+    //Search through the dataBase for the authUserId
+    //And fetch the channels for the respective authUser
+    
+    return {
+        channels: [
+            {
+                channelId: 1,
+                name: 'My Channel',
+            }
+        ],
+    };
 }
