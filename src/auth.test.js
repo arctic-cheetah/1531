f7854f24808695bf6fc23afdd0934d49f974c2ef
Joules_Patman <z5312813@ad.unsw.edu.au>
2023-03-03 14:28:23 +1100

    Renamed code to authRegisterV1

--- src/auth.test.js
+++ src/auth.test.js
@@ -17,27 +17,27 @@ describe('authRegisterV1', () => {
   //Error cases:
   test('Invalid email', () => {
     const check = {email : "hello.com", password: "gihsbeFFKLWN23", nameFirst: "Alison", nameLast: "Patman"};
-    expect(authRegister(check)).toStrictEqual(ERROR);
+    expect(authRegisterV1(check)).toStrictEqual(ERROR);
   });
   
   test('Address already used', () => {
     const check = {email : "Alison@hello.com", password: "gihsbeFFKLWN23", nameFirst: "Alison", nameLast: "Patman"};
-    expect(authRegister(check)).toStrictEqual(ERROR);
+    expect(authRegisterV1(check)).toStrictEqual(ERROR);
   });
   
   test('Length of password is less than 6', () => {
     const check = {email : "Alison@hello.com", password: "123456", nameFirst: "Alison", nameLast: "Patman"};
-    expect(authRegister(check)).toStrictEqual(ERROR);
+    expect(authRegisterV1(check)).toStrictEqual(ERROR);
   });
 
   test('firstName is not between 1 to 50 inclusive', () => {
     const check = {email : "Alison@hello.com", password: "123456", nameFirst: "E", nameLast: "Patman"};
-    expect(authRegister(check)).toStrictEqual(ERROR);
+    expect(authRegisterV1(check)).toStrictEqual(ERROR);
   
   });
   test('lastName is not between 1 to 50 inclusive', () => {
     const check = {email : "Alison@hello.com", password: "123456", nameFirst: "Alison", nameLast: "B"};
-    expect(authRegister(check)).toStrictEqual(ERROR);
+    expect(authRegisterV1(check)).toStrictEqual(ERROR);
   });
   
 
