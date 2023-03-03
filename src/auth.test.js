b0abf06ac350a479781c3c40617b69f9f6ce7fd0
Xiang Ren <z5353780@ad.unsw.edu.au>
2023-03-03 15:08:47 +1100

    add beforeEach funxtion

--- src/auth.test.js
+++ src/auth.test.js
@@ -8,12 +8,16 @@ import {
 const ERROR = {error : "error"};
 
 
+beforeEach(() => {
+  clearV1();
+})
 
 // Testing for authRegisterV1
 // Main case: If it works
 // Edge case: (Same name, but different email)
 // Error case: Below
 describe('authRegisterV1', () => {
+
   //Error cases:
   test('Invalid email', () => {
     const check = {email : "hello.com", password: "gihsbeFFKLWN23", nameFirst: "Alison", nameLast: "Patman"};
@@ -49,36 +53,44 @@ describe('authLoginV1', () => {
   //right cases:
 
   test ( 'Testing login', () => {
-    cleardata();
+
     const userRegister = authRegisterV1('joseph@unsw.edu.au', '123456', 'Joseph', 'Caspar');
     const userRegisterId = userRegister.userId;
+
     const userLogin = authLoginV1('joseph@unsw.edu.au', '123456');
     const userLoginId = userLogin.userId;
+
     expect(userLoginId).toBe(userRegisterId);
+
   });
 
   //Error cases:
 
   test ( 'Testing login - email that hasnt been used', () => {
-    cleardata();
+
     const userLogin = authLoginV1('jcasp@unsw.edu.au', 'gamer189');
     expect(userLogin).toStrictEqual({ERROR});
+
   });
 
   test ( 'Testing login - password not correct ' , () => {
-    cleardata();
+
     const userRegister = authRegisterV1('joseph@unsw.edu.au', '123456', 'Joseph', 'Caspar');
     const userRegisterPassword = userRegister.password;
+
     const userLogin = authLoginV1('joseph@unsw.edu.au', '123489');
     const userLoginPassword = userLogin.password;
+
     expect(userLogin).toStrictEqual({ERROR});
+
   });
 
 
   test( 'Testing Login - email entered does not belong to a user' , () => {
-    cleardata();
+
     const userRegister = authRegisterV1('joseph@unsw.edu.au', '123456', 'Joseph', 'Caspar');
     const check = AuthLoginV1( 'xiang.ren@unsw.edu.au' , '123456')
+    
     expect(check).to =toStrictEqual({ERROR});
   })
 
